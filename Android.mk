4414e14accd3367909c7dce2024e06d44e2c1033
jhenrique09 <jhsv09@gmail.com>
2022-02-10 15:53:05 -0300

    Refactor deps structure
    Change-Id: Ia6f141be94f70cb640479264396693c1a3dc1a35

--- Android.mk
+++ Android.mk
@@ -5,26 +5,16 @@ LOCAL_MODULE := FaceUnlockServiceAndroidStudio
 LOCAL_MODULE_CLASS := FAKE
 LOCAL_MODULE_SUFFIX := -timestamp
 
-faceunlock_framework_deps := $(call java-lib-deps,faceunlock_framework)
 faceunlock_android_framework_deps := $(call java-lib-deps,framework)
 
-prebuilt_libs_path := $(abspath $(LOCAL_PATH))/../../../external/faceunlock/prebuilt/lib64
-
-faceunlock_libs_path := $(abspath $(LOCAL_PATH))/app/libs
 faceunlock_system_libs_path := $(abspath $(LOCAL_PATH))/app/system_libs
-faceunlock_native_libs_path := $(abspath $(LOCAL_PATH))/app/src/main/jniLibs/arm64-v8a
 
 include $(BUILD_SYSTEM)/base_rules.mk
 
-$(LOCAL_BUILT_MODULE): $(faceunlock_framework_deps) $(faceunlock_android_framework_deps)
-	$(hide) mkdir -p $(faceunlock_libs_path)
+$(LOCAL_BUILT_MODULE): $(faceunlock_android_framework_deps)
 	$(hide) mkdir -p $(faceunlock_system_libs_path)
-	$(hide) mkdir -p $(faceunlock_native_libs_path)
-	$(hide) rm -rf $(faceunlock_libs_path)/*.jar
 	$(hide) rm -rf $(faceunlock_system_libs_path)/*.jar
-	$(hide) cp $(faceunlock_framework_deps) $(faceunlock_libs_path)/faceunlock_framework.jar
 	$(hide) cp $(faceunlock_android_framework_deps) $(faceunlock_system_libs_path)/framework.jar
-	$(hide) cp $(prebuilt_libs_path)/*.so $(faceunlock_native_libs_path)
 	$(hide) echo "Fake: $@"
 	$(hide) mkdir -p $(dir $@)
 	$(hide) touch $@
